ff4036a4bd53bacf433481b3f478770154233efe
Bahoora <baheradawy@gmail.com>
2026-05-07 15:46:47 +0300

    .

--- Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/config/JacksonConfig.java
+++ Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/config/JacksonConfig.java
@@ -1,25 +1,24 @@
 package grad_project.seasonal_job_matching.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     @Primary
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        return builder -> {
-            // Force Spring Web to load the Java 8 Time Module
-            builder.modules(new JavaTimeModule());
-            builder.simpleDateFormat("dd-MM-yyyy HH:mm:ss");
-
-            // Prevent Jackson from turning dates into weird number arrays
-            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        };
+    public ObjectMapper objectMapper() {
+        return new Jackson2ObjectMapperBuilder()
+                .modules(new JavaTimeModule()) // Teaches Jackson about Java 8 Dates
+                .simpleDateFormat("dd-MM-yyyy HH:mm:ss")
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build(); // Builds the actual engine
     }
+
 }
