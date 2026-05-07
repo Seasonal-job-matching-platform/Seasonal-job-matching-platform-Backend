1ccce823e8a6fc857132383d7b075075454699e4
Bahoora <baheradawy@gmail.com>
2026-05-07 15:20:55 +0300

    Added more formatting of date in job comments DTO

--- Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/dto/responses/JobCommentResponseDTO.java
+++ Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/dto/responses/JobCommentResponseDTO.java
@@ -3,6 +3,12 @@ package grad_project.seasonal_job_matching.dto.responses;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import lombok.Data;
 
 @Data
@@ -14,5 +20,8 @@ public class JobCommentResponseDTO {
     private String userName;
     private LocalDateTime createdAt;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private List<JobCommentResponseDTO> replies;
 }
