353f0a4c76a64480619ac336936626bd6601582b
Bahoora <baheradawy@gmail.com>
2026-05-07 15:39:34 +0300

    Override spring default date settings

--- Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/config/JacksonConfig.java
+++ Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/config/JacksonConfig.java
@@ -5,20 +5,20 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
+    @Primary
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
         return builder -> {
-            // 1. Force Spring Web to load the Java 8 Time Module
+            // Force Spring Web to load the Java 8 Time Module
             builder.modules(new JavaTimeModule());
-
-            // 2. Set the global date format for every endpoint in your app
             builder.simpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
-            // 3. Prevent Jackson from turning dates into weird number arrays
+            // Prevent Jackson from turning dates into weird number arrays
             builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         };
     }
